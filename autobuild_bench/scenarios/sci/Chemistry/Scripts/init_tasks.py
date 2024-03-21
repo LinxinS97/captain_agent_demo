5b3a56e9bc758efdfa5fee34ed806e179452652d
jmliu <jmliu206@gmail.com>
2024-03-21 15:45:59 +0900

    [update] sci chem

--- autobuild_bench/scenarios/sci/Chemistry/Scripts/init_tasks.py
+++ autobuild_bench/scenarios/sci/Chemistry/Scripts/init_tasks.py
@@ -17,7 +17,6 @@ SCRIPT_DIR = os.path.dirname(SCRIPT_PATH)
 SCENARIO_DIR = os.path.realpath(os.path.join(SCRIPT_DIR, os.path.pardir))
 TEMPLATES_DIR = os.path.join(SCENARIO_DIR, "Templates")
 TASKS_DIR = os.path.join(SCENARIO_DIR, "Tasks")
-DOWNLOADS_DIR = os.path.join(SCENARIO_DIR, "Downloads")
 SAVE_DIR = os.path.join(SCENARIO_DIR, "Saved_agents")
 
 SELECTED_CHEM_PROBLEMS = [
