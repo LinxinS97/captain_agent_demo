b0f4efabf6824e8435038a29dfd6cf5b58233cb5
Linxin Song <rm.social.song1@gmail.com>
2024-04-03 10:28:48 +0000

    modify videoqa name

--- autogen/agentchat/contrib/capabilities/video_qa.py
+++ autogen/agentchat/contrib/capabilities/video_qa.py
@@ -21,7 +21,7 @@ pip install flash-attn --no-build-isolation
 pip install decord opencv-python git+https://github.com/facebookresearch/pytorchvideo.git@28fe037d212663c6a24f373b94cc5d478c8c1a1d
 """
 
-def main(video_path: str, question: str):
+def video_qa(video_path: str, question: str):
     """
     TODO: tool description
     """
